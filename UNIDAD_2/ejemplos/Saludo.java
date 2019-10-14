a638dab6efdd507a1b2eb522ee852c72a2166512
luismrtic <48987165+luismrtic@users.noreply.github.com>
2019-10-14 12:15:55 +0200

    Update Saludo.java

--- UNIDAD_2/ejemplos/Saludo.java
+++ UNIDAD_2/ejemplos/Saludo.java
@@ -1,13 +1,11 @@
 package psp_hilos;
 /*Crea un hilo implementando la interfaz Runnable*/
 
-//@author IMCG
-
 public class Saludo implements Runnable {
 //clase que implementa a Runnable
     public void run() {
-    //se redefine el método run()con el código asociado al hilo
-        System.out.println("�Saludo desde un hilo creado con Runnable!");
+    //se redefine el mÃ©todo run()con el cÃ³digo asociado al hilo
+        System.out.println("¡Saludo desde un hilo creado con Runnable!");
     }
     public static void main(String args[]) {
        Saludo  miRunnable=new Saludo();
@@ -16,7 +14,7 @@ public class Saludo implements Runnable {
        //se crea un objeto Thread (el hilo hilo1) pasando como argumento
       // al constructor un objeto Saludo
         hilo1.start();
-       //se invoca al método start() del hilo hilo1
+       //se invoca al mÃ©todo start() del hilo hilo1
     }
 }
 
